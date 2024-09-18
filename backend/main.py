0e008faa0cf173e9f498a056172e1e1ede31a000
RENZ-MAVERICK-FAMA <102272777+RENZ-MAVERICK-FAMA@users.noreply.github.com>
2024-09-18 15:22:19 +0800

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from flask_mail import Mail
 from website import create_app
 from flask_cors import CORS
-from flask import Flask, request, has_request_context
+from flask import request, has_request_context
 from flask_jwt_extended import JWTManager
 import logging
 from logging.handlers import RotatingFileHandler
@@ -22,17 +22,19 @@ class newFormatter(logging.Formatter):
 
 logFormatter = newFormatter("%(asctime)s - %(url)s - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
 
+# Console handler
 consoleHandler = logging.StreamHandler()
-consoleHandler.setFormatter(logFormatter)  # Use logFormatter, not newFormatter
+consoleHandler.setFormatter(logFormatter)
 logger.addHandler(consoleHandler)
 
-filehandler = RotatingFileHandler("logs.log" ,backupCount=500 ,maxBytes=20883)
-filehandler.setFormatter(logFormatter)  # Use logFormatter, not newFormatter
-logger.addHandler(filehandler)
+# File handler
+fileHandler = RotatingFileHandler("logs.log", maxBytes=1024 * 1024 * 5, backupCount=5)  # 5 MB per file, keep 5 backups
+fileHandler.setFormatter(logFormatter)
+logger.addHandler(fileHandler)
 
-CORS(app, resources={r"/*": {"origins": "http://main--qrmc-pass.netlify.app"}})
+CORS(app, resources={r"/*": {"origins": "https://main--qrmc-pass.netlify.app"}})
 jwt = JWTManager(app)
 mail = Mail(app)
 
-if __name__ == '__main__':  
+if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=9000)
